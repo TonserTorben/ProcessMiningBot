14f7b086dbcb17f83e6f9bc11461f00453b25331
Rasmu <rasmus_blumensaadt@hotmail.com>
2019-12-15 21:26:47 +0100

    Added hash size, c-string and folder to globals

--- Globals.py
+++ Globals.py
@@ -4,11 +4,16 @@ from Enums.execution_enum import ExecutionState
 
 from ContextManager.ContextManager import ContextManager
 #Should contain all global variables e.g. 
+from decouple import config
+
+CONNECTIONSTRING = config('CONNECTIONSTRING')
+FILES_FOLDER = config('FILES_FOLDER')
+HASH_BLOCK_SIZE = config('HASH_BLOCK_SIZE')
+R_SCRIPT = config('R_SCRIPT')
 
 _botstate = BotState.Idle
 _execution_state = ExecutionState.Nothing
-_context_manager = None
-
+_context_manager = ContextManager(CONNECTIONSTRING, FILES_FOLDER, HASH_BLOCK_SIZE)
 
 def get_bot_state():
     return _botstate
@@ -32,9 +37,9 @@ def set_execution_state(state):
     global _execution_state
     _execution_state = state
 
-def init_context_manager(connectionString):
+def init_context_manager(connection_string, files_folder, hash_block_size):
     global _context_manager
-    _context_manager = ContextManager(connectionString)
+    _context_manager = ContextManager(connection_string, files_folder, hash_block_size)
 
 def get_context_manager():
     return _context_manager
@@ -42,3 +47,5 @@ def get_context_manager():
 def set_context_manager(connectionstring):
     pass
 
+def get_r_script():
+    return R_SCRIPT
