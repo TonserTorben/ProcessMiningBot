a8d118eedab6868416b542dfe38fd905eabb6168
Rasmu <rasmus_blumensaadt@hotmail.com>
2019-12-15 21:28:36 +0100

    Altered insert_file to insert in both tables

--- ContextManager/DB_Handler.py
+++ ContextManager/DB_Handler.py
@@ -12,6 +12,7 @@ class DB_Handler:
     def get_item(self, db_select, db_from, db_where):
         #Idea is to specify the specific table, what item to get and the clauses for the select statement.
         statement = "SELECT {} FROM {} WHERE {}".format(db_select, db_from, db_where)
+        print(statement)
         self.c.execute(statement)
         result = self.c.fetchone()
         result = result[0] if result != None else None
@@ -36,17 +37,21 @@ class DB_Handler:
 
     #Insert STATEMENTS
     # Inserting file in both Files table and Files_in_chat table
-    def insert_file(self, file):
-        statement = "INSERT INTO Files (Name, File, User, Date_of_upload, Last_Edited, Filtered, Size, Type) Values(?, ?, ?, ?, ?, ?, ?, ?)"
-        values = (file['name'], file['file'], file['user'], file['date_of_upload'], file['last_edit'], file['filter'], file['size'], file['type'])
+    def insert_file(self, file_hash, chat_id, user, date, name, file_id=None, path=None, size=None, type=None):
+        if file_id == None:
+            statement = "INSERT INTO Files (Hash, File_path, Size, Type) Values(?, ?, ?, ?)"
+            values = (file_hash, path, size, type)
+            self.c.execute(statement, values)
+            self.db.commit()
+            file_id = self.c.lastrowid
+        else:
+            file_id = file_id
+        statement = "INSERT INTO Files_in_Chat (Chat_id, File_id, User, Upload_date, File_name) VALUES(?, ?, ?, ?, ?)"
+        values = (chat_id, file_id, user, date, name)
         self.c.execute(statement, values)
         self.db.commit()
-        row_id = self.c.lastrowid
-        statement = "INSERT INTO Files_in_Chat (Chat_id, File_id) VALUES(?, ?)"
-        values = (file['chat_id'], row_id)
-        self.c.execute(statement, values)
-        self.db.commit()
-        return row_id
+        file_in_chat_id = self.c.lastrowid
+        return file_in_chat_id
 
     def insert_script(self, script):
         statement = "INSERT INTO Scripts (Name, Language, Script, Output) VALUES(?, ?, ?, ?)"
